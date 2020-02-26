0faae8a2c900c6336700760d4f70d648a6d6b2cf
Immanuel Albrecht <immanuel@perazim.ch>
2020-02-26 16:35:37 +0100

    Finished Input-Output exercise with InOut.c and added a makefile for exercise 2.

---
+++ comp_phys/ex2/src/InOut.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	double a;
+	double b;
+	
+	printf("Dieses Programm gibt die Lösung fuer die Aufgabe ax + b = 0 aus. \n");
+	printf("a = ");
+	scanf("%lf", &a);
+	printf("b = ");
+	scanf("%lf", &b);
+	
+	double x = -b / a;
+	
+	printf("Die Loesung ist x = %lf", x);
+	
+	return 0;
+}
