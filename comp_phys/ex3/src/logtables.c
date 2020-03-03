1bacd9ed5d7dbbf9a3077e88f0babc7034a1a3eb
Immanuel Albrecht <immanuel@perazim.ch>
2020-03-03 21:56:54 +0100

    Finished the logtables program of the 3. Exercise.

--- comp_phys/ex3/src/logtables.c
+++ comp_phys/ex3/src/logtables.c
@@ -4,45 +4,98 @@
 
 /* The idea is to return the operation tables of the functions and, or, nand, nor and not*/
 
-int not_table() {
-    printf("%-5s | %-5s\n", "a", "!a");
-    for(int a=0; a<2; a++) {
-        printf("%-5s | %-5s\n", a, !a);
-    }
-    return 0;
-}
+void not_table();
+void and_table();
+void or_table();
+void nand_table();
+void nor_table();
 
 int main() {
-    const int num_ops = 1;
-    const char* operators[] = {"not", "something_else"};
+    const char* ops[] = {"not", "and", "or", "nand", "nor"};
+    const int num_ops = sizeof(ops)/sizeof(*ops);
     char* input;
 
     printf("This program returns the logical tables for some basic logical operators.\n");
     printf("Currently supported are: ");
     for(int i=0; i<num_ops; i++) {
-        printf("%s ", operators[i]);
+        printf("\"%s\" ", ops[i]);
     }
 
     printf("\n");
     printf("What operator table do you want to see?\n");
     scanf("%s", input);
 
-    printf("Something else: %s\n", operators[1]);
-    printf("%s\n", input);
-
-    int index;
-
-    for(int i=0; i<num_ops; i++) {
-        printf("%d", index);
-
-        printf("%d", strcmp(input, operators[i]));
-        /*
-        if(strcmp(input,operators[i])) {
-            printf("You chose %s", operators[i]);
-            index = i;
-        }
-        */
+    if(strcmp(input, ops[0]) == 0) {
+        not_table();
+    }
+    else if(strcmp(input, ops[1]) == 0) {
+        and_table();
+    }
+    else if(strcmp(input, ops[2]) == 0) {
+        or_table();
+    }
+    else if(strcmp(input, ops[3]) == 0) {
+        nand_table();
+    }
+    else if(strcmp(input, ops[4]) == 0) {
+        nor_table();
+    }
+    else {
+        printf("You really wanna see it all!\n");
+        not_table();
+        and_table();
+        or_table();
+        nand_table();
+        nor_table();
     }
 
     return 0;
+}
+void not_table() {
+    printf("The \"not\"-table:\n");
+    printf("%-3s | %-3s\n", "a", "!a");
+    for(int a=0; a<2; a++) {
+        printf("%-3d | %-3d\n", a, !a);
+    }
+    return;
+}
+void and_table() {
+    printf("The \"and\"-table:\n");
+    printf("%-3s | %-3s | %-3s\n","a","b", "a and b");
+    for(int a=0; a<2; a++) {
+        for(int b=0; b<2; b++) {
+            printf("%-3d | %-3d | %-3d\n", a, b, a&&b);
+        }
+    }
+    return;
+}
+void or_table() {
+    printf("The \"or\"-table:\n");
+    printf("%-3s | %-3s | %-3s\n","a","b", "a or b");
+    for(int a=0; a<2; a++) {
+        for(int b=0; b<2; b++) {
+            printf("%-3d | %-3d | %-3d\n", a, b, a||b);
+        }
+    }
+    return;
+}
+void nand_table() {
+    printf("The \"nand\"-table:\n");
+    printf("%-3s | %-3s | %-3s\n","a","b", "a or b");
+    for(int a=0; a<2; a++) {
+        for(int b=0; b<2; b++) {
+            printf("%-3d | %-3d | %-3d\n", a, b, !(a&&b));
+        }
+    }
+    return;
+}
+void nor_table() {
+    printf("The \"nor\"-table:\n");
+    printf("%-3s | %-3s | %-3s\n","a","b", "a or b");
+    for(int a=0; a<2; a++) {
+        for(int b=0; b<2; b++) {
+            printf("%-3d | %-3d | %-3d\n", a, b, !(a||b));
+        }
+    }
+    return;
 }
