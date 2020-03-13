6613e361543f424effe44b1358df3ce7d8ffdfff
Immanuel Albrecht <immanuel@perazim.ch>
2020-03-13 16:20:45 +0100

    Fixed the throwing_stone.c program.

--- comp_phys/ex4/src/throwing_stone.c
+++ comp_phys/ex4/src/throwing_stone.c
@@ -3,60 +3,51 @@
 #include <math.h>
 #include "myfuncs.h"
 
-int verbose = 1;
-
-double* read_stats() {
-    /* Reads in the angle, height and velocity of the throw in an double arr[3]*/
-    static double stats[3] = {0, 0, 0};
-
-    printf("Hello. You are here to throw a stone.\n");
-    printf("At which angle do you want to throw it? (deg)\n");
-    scanf("%lf", &stats[0]);
-    printf("How far above the ground are you? (meters)\n");
-    scanf("%lf", &stats[1]);
-    printf("And how fast do you throw it? (meters/second)\n");
-    scanf("%lf", &stats[2]);
-
-    return stats;
+void read_data(double* angleptr, double* velocityptr, double* heightptr) {
+    do {
+        printf("At which angle do you want to throw it? (deg)\n");
+        scanf("%lf", angleptr);
+    } while(fabs(*angleptr) > 90);
+    do {
+        printf("How fast do you throw it? (meters/second)\n");
+        scanf("%lf", velocityptr);
+    } while(*velocityptr <= 0);
+    do {
+        printf("And how high above the ground are you? (meters)\n");
+        scanf("%lf", heightptr);
+    } while(*heightptr < 0);
+    return;
 }
 
-double throw(double* ptr, int ptr_len) {
-    double distance = 0;
-    double angle = *ptr, height = *(ptr+1), velocity = *(ptr+2);
-    double vx = velocity*cos(angle);
-    double vy = velocity*sin(angle);
-    double g = 9.86;
+void throw(double angle, double velocity, double height, double* distanceptr, int notes) {
+    double vy = velocity*sin(angle), vx = velocity*cos(angle), g = 9.81;
 
-    double time = quadratic(-g/2, vy, height);
+    double time = quadratic(-g/2., vy, height);
 
-    float step_len = 0.1;
-    for(int i=1; i*step_len<time; i++) {
-        double step = i*step_len;
-        printf("%d. (x, y): (%lf, %lf)\n", i, vx*step, height + vy*step + g/2*pow(step,2));
+    if(notes > 0) {
+        double interval = time/(notes+1.);
+        for(int i = 1; i< (notes+1); i++) {
+            printf("(x, y): (%lf, %lf)\n", vx*interval*i+(*distanceptr), height + vy*interval*i - g/2.*pow(i*interval,2.));
+        }
     }
-    return vx*time;
+
+    *distanceptr += vx*time;
+    return;
 }
 
 int main(int argc, char* argv[]) {
-    double stats[3]; /* angle, height, velocity */
-    int stats_len = 3;
-    if(argc == 4) {
-        verbose = 0;
-        for(int i=1; i<argc; i++){
-            stats[i-1] = atof(argv[i]);
-        }
-    }
-    else {
-        double* temp = read_stats();
-        for(int i=0; i<stats_len; i++){
-            stats[i] = *(temp+i);
-        }
-    }
+    double angle = 0, velocity = 0, height = 0, distance = 0;
+    int notes = 0;
 
-    stats[0] = stats[0] * M_PI / 180;
+    if(argc == 2) notes = atoi(argv[1]);
+    read_data(&angle, &velocity, &height);
+    angle = angle*M_PI/180.;
 
-    double distance = throw(stats, stats_len);
-    printf("You threw it as far as %lf meters!", distance);
+    printf("Angle: %lf, Velocity: %lf, Height %lf\n", angle, velocity, height);
+
+    throw(angle, velocity, height, &distance, notes);
+
+    printf("Distance: %lf", distance);
 
     return 0;
 }
