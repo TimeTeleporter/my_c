ce3b0dc390f315fd21e40a8bdc986e2ebfbf2350
Immanuel Albrecht <immanuel@perazim.ch>
2020-03-21 22:33:07 +0100

    Now the Ex5 is finished.

--- comp_phys/ex5/src/file_read.c
+++ comp_phys/ex5/src/file_read.c
@@ -4,10 +4,20 @@
 int main() {
     FILE* ptr = fopen("power_series.txt", "r");
 
-    int array[10];
-    for(int i = 0; i<10; i++) fscanf(ptr, "%d", array+i);
+    if(ptr == NULL) {
+        printf("No file anywhere to be seen.");
+        return 1;
+    }
 
-    for(int i = 0; i<10; i++) printf("%d\n", array+i);
+    int array[10];
+
+    int i = 0;
+    while(feof(ptr) == 0) {
+        fscanf(ptr, "%d", array+i);
+        i++;
+    }
+
+    for(int j = 0; j<i; j++) printf("%d\n", *(array+j));
 
     fclose(ptr);
     return 0;
